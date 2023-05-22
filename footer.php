f310d1c408c24dd16fa88d4392c778d399d44dca
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-22 15:21:04 +0200

    Import

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * The template for displaying the footer
+ * 
+ * @package Day Six theme
+ */
+ ?>
+
+
+
+<?php wp_footer('my_custom_js'); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * The main template file
+ * 
+ * @package Day Six theme
+ */
+
+
+get_header(); ?>
+
+<!-- BLOCK: MAIN -->
+<main class="relative">
+   
+
+
+</main>
+<?php get_footer(); ?>
