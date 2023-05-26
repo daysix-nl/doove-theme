5e1b2bbc223342b3bd85080f7304ee9fee2f0c2b
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-26 13:55:15 +0200

    WIP

--- header.php
+++ header.php
@@ -24,22 +24,25 @@
     <a href="/" class="ml-2 md:ml-4 w-[231px]"><?php include get_template_directory() . '/img/icons/logo.php'; ?></a>
     <div class="flex h-[82px] md:h-[71px] lg:h-[68px] items-center">
       <div class="hidden lg:flex">
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 flex items-center hover:opacity-[0.7] duration-300">Zorghulpmiddelen <span class="lg:h-[4.689px] xl:h-[5px] ml-2"><?php include get_template_directory() . '/img/icons/arrow-down.php'; ?></span></a>
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Facility Support</a>
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Academie</a>
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Nieuws</a>
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Vacatures</a>
-        <a href="#" class="lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Contact</a>
+        <button id="zorghulpmiddelen" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 flex items-center hover:opacity-[0.7] duration-300 inactive">Zorghulpmiddelen <span class="lg:h-[4.689px] xl:h-[5px] ml-2"><?php include get_template_directory() . '/img/icons/arrow-down.php'; ?></span></button>
+        <a href="#" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Facility Support</a>
+        <a href="#" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Academie</a>
+        <a href="#" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Nieuws</a>
+        <a href="#" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Vacatures</a>
+        <a href="#" class="nohvr lg:text-13 lg:leading-18 xl:text-16 xl:leading-22 font-nunito font-semibold text-[#726A63] ml-4 hover:opacity-[0.7] duration-300">Contact</a>
       </div>
       <div class="hidden lg:flex ml-4 lg:mr-[15px] xl:mr-2">
-        <a href="" class="uppercase bg-orange rounded-[2px] lg:text-14 lg:leading-17 xl:text-18 xl:leading-22 font-nunito font-semibold text-white hover:opacity-[0.7] duration-300 lg:w-[78px] lg:h-[34px] xl:h-[39px] xl:w-[88px] flex items-center justify-center">Kis</a>
-        <a href="" class="uppercase bg-[#F4F4F5] rounded-[2px] lg:text-14 lg:leading-17 xl:text-18 xl:leading-22 font-nunito font-semibold text-white hover:opacity-[0.7] duration-300 lg:w-[36px] lg:h-[34px] xl:h-[39px] xl:w-[40px] flex items-center justify-center lg:ml-[10px] xl:ml-[15px]"><span class="lg:h-[12.9px] xl:h-[14.5px]"><?php include get_template_directory() . '/img/icons/glass.php'; ?></span></a>
+        <button id="kis" class="uppercase bg-orange rounded-[2px] lg:text-14 lg:leading-17 xl:text-18 xl:leading-22 font-nunito font-semibold text-white hover:opacity-[0.7] duration-300 lg:w-[78px] lg:h-[34px] xl:h-[39px] xl:w-[88px] flex items-center justify-center inactive">Kis</button>
+        <button id="search" class="uppercase bg-[#F4F4F5] rounded-[2px] lg:text-14 lg:leading-17 xl:text-18 xl:leading-22 font-nunito font-semibold text-white hover:opacity-[0.7] duration-300 lg:w-[36px] lg:h-[34px] xl:h-[39px] xl:w-[40px] flex items-center justify-center lg:ml-[10px] xl:ml-[15px] inactive"><span class="lg:h-[12.9px] xl:h-[14.5px]"><?php include get_template_directory() . '/img/icons/glass.php'; ?></span></button>
       </div>
       <div class="block lg:hidden ml-4 mr-2 md:mr-[25px]">
-        <a href="" class="uppercase bg-orange rounded-[2px] w-[49px] h-[47px] md:h-[40px] md:w-[42px] flex items-center justify-center"></a>
+        <button id="hamburger" class="uppercase bg-orange rounded-[2px] w-[49px] h-[47px] md:h-[40px] md:w-[42px] flex items-center justify-center inactive"></button>
       </div>
     </div>
   </div>
 </header>
   
+<!-- OVERLAY ZORGHULPMIDDELEN -->
+<div id="zorghulpmiddelen" class="nohvr left-0 right-0 top-0 h-[500px] bg-white fixed z-40 menuitemoverlay inactive">
 
+</div>
--- script/index.js
+++ script/index.js
@@ -11,3 +11,45 @@ window.addEventListener('resize', () => {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+
+// BUTTON ELEMENTEN
+// Zoek alle button-elementen op de pagina
+var buttons = document.getElementsByTagName('button');
+var hoverElements = document.getElementsByClassName('nohvr');
+
+
+// Itereer over alle gevonden button-elementen
+for (var i = 0; i < buttons.length; i++) {
+  // Voeg eventlisteners toe voor hover-gebeurtenissen op buttons
+  buttons[i].addEventListener('mouseenter', function () {
+    // Toggle de classes wanneer de muis over de button beweegt
+    toggleClasses(this);
+  });
+
+}
+
+// Functie om classes te toggelen en andere elementen met hetzelfde ID bij te werken
+function toggleClasses(element) {
+  // Toggle de classes voor het huidige element
+  element.classList.toggle('inactive');
+  element.classList.toggle('active');
+  element.classList.toggle('nohvr'); // Toggle de class .hvr naar .nohvr
+
+  // Controleer of er andere elementen zijn met hetzelfde ID
+  var elementsWithSameId = document.querySelectorAll('[id="' + element.id + '"]');
+  for (var k = 0; k < elementsWithSameId.length; k++) {
+    // Sla het huidige element over (om duplicatie van classes te voorkomen)
+    if (elementsWithSameId[k] !== element) {
+      // Toggle de classes voor de andere elementen
+      elementsWithSameId[k].classList.toggle('inactive');
+      elementsWithSameId[k].classList.toggle('active');
+      elementsWithSameId[k].classList.toggle('nohvr'); // Toggle de class .hvr naar .nohvr
+    }
+  }
+}
+
+
+
+
+
+
