bb0265bb7f87425f3f637d445b936f43a1a29ee8
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-08-15 17:00:48 +0200

    wip

--- blocks/acf-template-product/acf-template-product.php
+++ blocks/acf-template-product/acf-template-product.php
@@ -195,7 +195,7 @@ else: ?>
 
                               <a href="<?php the_permalink();?>" class="col-span-1 md:last-of-type:hidden lg:last-of-type:block">
                                     <div class="w-full aspect-square bg-[#F4F4F4] flex items-center justify-center">
-                                        <img class="w-auto h-auto max-w-[133px] max-h-[133px] md:max-w-[170px] md:max-h-[170px] lg:max-w-[215px] lg:max-h-[215px] xl:max-w-[242px] xl:max-h-[242px]" src="/wp-content/themes/doove-theme/img/local/image129.png" alt="">
+                                        <img class="w-auto h-auto max-w-[133px] max-h-[133px] md:max-w-[170px] md:max-h-[170px] lg:max-w-[215px] lg:max-h-[215px] xl:max-w-[242px] xl:max-h-[242px]" src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>">
                                     </div>
                                     <h3 class="font-nunito font-bold text-16 leading-20 lg:text-16 lg:leading-25 xl:text-18 xl:leading-30 text-[#332E2A] mt-[15px] mb-1 md:mt-2 md:mb-[10px]"><?php the_title();?></h3>
                                     <p class="font-nunito font-normal text-[#332E2A] text-14 leading-20 lg:text-15 lg:leading-24 xl:text-16 xl:leading-27"><?php the_field('introductie_tekst', $post_id);?></p>
