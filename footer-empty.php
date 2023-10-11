b31edd00d7e350de3766e0e9c8488f10614d9362
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-10-11 16:16:31 +0200

    Update footer-empty.php

--- footer-empty.php
+++ footer-empty.php
@@ -6,6 +6,24 @@
  */
  ?>
 
+
+<script type='text/javascript'>
+
+  (function(d,t) {
+
+    _scoopi = {'onload': function() { this.trkDocumentLoad(); }};
+
+    var s=d.getElementsByTagName(t)[0], js=d.createElement(t); js.async=1;
+
+    js.src='//api.salesfeed.com/v3/bootstrap.js?aid=doove18';
+
+    s.parentNode.insertBefore(js,s);
+
+  })(document,'script');
+
+</script>
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
