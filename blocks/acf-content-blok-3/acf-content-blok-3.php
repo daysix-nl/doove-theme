5d87841f73d58006d85ef1fdb430350af4617d73
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-11-02 20:12:07 +0100

    wip

--- blocks/acf-content-blok-3/acf-content-blok-3.php
+++ blocks/acf-content-blok-3/acf-content-blok-3.php
@@ -4,12 +4,12 @@ if (isset($block['data']['preview_image_help'])): /* rendering in inserter previ
 else: ?>
     <!-- CONTENT BLOK 3 -->
     <section class="bg-<?php the_field('achtergrond_kleur');?> <?php the_field('padding_top');?> <?php the_field('padding_bottom');?>">
-        <div class="container flex flex-col lg:flex-row items-center justify-between space-y-[40px]">
+        <div class="container flex flex-col lg:flex-row items-center justify-between">
             <div class="col-span-1 grid gap-[25px] md:gap-3 lg:gap-4 order-2">
                 <h2 class="text-22 leading-33 md:text-21 md:leading-28 lg:text-22 lg:leading-29 xl:text-25 xl:leading-33 font-satoshi text-<?php the_field('titel_kleur');?> font-semibold lg:max-w-[439px] xl:max-w-[527px]"><?php the_field('titel');?></h2>
                 <div class="text-18 leading-30 md:text-14 md:leading-24 lg:text-15 lg:leading-25 xl:text-18 xl:leading-30 font-nunito text-black font-normal lg:max-w-[439px] xl:max-w-[527px] text-editor"><?php the_field('tekst');?></div>                
             </div>
-            <div class="col-span-1 block md:hidden lg:block pt-0 md:pt-0 <?php the_field('order');?> w-full lg:max-w-[545px] xl:max-w-[619px]">
+            <div class="col-span-1 block md:hidden lg:block py-2 md:py-0 <?php the_field('order');?> w-full lg:max-w-[545px] xl:max-w-[619px]">
                 <iframe class="aspect-video w-full" src="https://www.youtube-nocookie.com/embed/<?php the_field('id_youtube');?>?controls=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowfullscreen></iframe>
             </div>
         </div>
