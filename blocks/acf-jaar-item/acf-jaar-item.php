ca5b995594bbcb995ec2257a36f6380ba0d9cc40
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-12-04 10:20:48 +0100

    wip

---
+++ blocks/acf-jaar-item/acf-jaar-item.php
@@ -0,0 +1,62 @@
+<?php
+if (isset($block['data']['preview_image_help'])): /* rendering in inserter preview  */
+    echo '<img src="' . $block['data']['preview_image_help'] . '" style="width:100%; height:auto;">';
+else: ?>
+    <!-- JAAR ITEM -->
+    <section class="h-[738px] md:h-[933px] lg:h-[632px] xl:h-[722px] overflow-hidden relative">
+        <div class="z-[2] relative h-full w-full flex flex-col md:flex-row justify-between px-[20px] md:px-[unset] max-w-[390px] md:max-w-[604px] lg:max-w-[911px] xl:max-w-[1068px] mx-auto items-center">
+            <div class="grid items-center h-full">
+                <div class="w-full md:w-[381px] lg:w-[412px] xl:w-[490px] grid h-fit">
+                    <h3 class="order-1 font-satoshi font-medium italic text-[#ED701F] text-76 leading-56 md:text-74 md:leading-54 lg:text-75 lg:leading-55 xl:text-80 xl:leading-60 pt-[5px] md:pt-[unset]">1966</h3>
+                    <h2 class="order-2 font-satoshi font-bold text-25 leading-35 text-[#332E2A] md:hidden mt-[40px]">Eerste stappen</h2>
+                    <div class="order-4 md:order-2 w-[350px] h-[211px] md:w-[382px] md:h-[220px] lg:w-[412px] lg:h-[239px] xl:w-[490px] xl:h-[284px] bg-black mt-[65px] md:mt-[52px] lg:mt-[50px] xl:mt-[45px] overflow-hidden">
+                        <img src="/wp-content/themes/doove-theme/img/local/1966.png" alt="" class="w-full h-full object-cover object-center-center">
+                    </div>
+                    <p class="order-3 md:order-3 font-nunito font-normal text-18 leading-30 md:text-14 md:leading-23 lg:text-15 lg:leading-25 xl:text-18 xl:leading-30 mt-3 md:max-h-[155px] lg:max-h-[unset] overflow-hidden">Jan Doove richt aan de Bergse Linker Rottekade in Rotterdam de Firma Doove op; een leverancier van rolstoelen en revalidatie hulpmiddelen.<br><br>Zijn vrouw Kitty verzorgt de administratie.</p>
+                </div>
+            </div>
+            <div class="w-[2px] ml-[8px] lg:ml-[unset] h-full relative hidden md:block">
+                <div class="bg-[#ED701F] w-[2px] absolute top-[50%] bottom-[0%] left-0 right-0"></div>
+                <div class="absolute bg-[#ED701F] w-[9px] h-[9px] mx-auto rounded-full top-[calc(50%-4.5px)] left-[-3.5px]"></div>
+            </div>
+            <div class="w-full md:w-[146px] lg:w-[412px] xl:w-[490px] hidden md:block">
+                <h2 class="font-satoshi font-bold md:text-21 md:leading-28 lg:text-22 leading-31 xl:text-25 xl:leading-35 text-[#332E2A]">Eerste stappen</h2>
+            </div>
+        </div>
+        <div class="absolute top-0 left-0 right-0 bottom-0 z-[0]">
+            <img src="/wp-content/themes/doove-theme/img/local/test.png" alt="" class="min-h-full min-w-full object-cover object-center-center opacity-[0.2]">
+        </div>
+         <div class="absolute top-0 left-0 right-0 bottom-0 z-[1]">
+            <div class="h-full w-full bg-white opacity-[0.17]"></div>
+        </div>
+    </section>
+
+     <section class="h-[738px] md:h-[933px] lg:h-[632px] xl:h-[722px] overflow-hidden relative">
+        <div class="z-[2] relative h-full w-full flex flex-col md:flex-row justify-between px-[20px] md:px-[unset] max-w-[390px] md:max-w-[604px] lg:max-w-[911px] xl:max-w-[1068px] mx-auto items-center">
+            <div class="order-1 lg:order-3 grid items-center h-full">
+                <div class="w-full md:w-[381px] lg:w-[412px] xl:w-[490px] grid h-fit">
+                    <h3 class="order-1 font-satoshi font-medium italic text-[#ED701F] text-76 leading-56 md:text-74 md:leading-54 lg:text-75 lg:leading-55 xl:text-80 xl:leading-60 pt-[5px] md:pt-[unset]">1976</h3>
+                    <h2 class="order-2 font-satoshi font-bold text-25 leading-35 text-[#332E2A] md:hidden mt-[40px]">Doove Ripple Services BV</h2>
+                    <div class="order-4 md:order-2 w-[350px] h-[211px] md:w-[382px] md:h-[220px] lg:w-[412px] lg:h-[239px] xl:w-[490px] xl:h-[284px] bg-black mt-[65px] md:mt-[52px] lg:mt-[50px] xl:mt-[45px] overflow-hidden">
+                        <img src="/wp-content/themes/doove-theme/img/local/1966.png" alt="" class="w-full h-full object-cover object-center-center">
+                    </div>
+                    <p class="order-3 md:order-3 font-nunito font-normal text-18 leading-30 md:text-14 md:leading-23 lg:text-15 lg:leading-25 xl:text-18 xl:leading-30 mt-3 max-h-[182px] md:max-h-[155px] lg:max-h-[unset] overflow-hidden">De familie Doove reist jaarlijks naar vakbeurzen in Engeland en komt daar met enige regelmaat nieuwe producten tegen voor de Nederlandse markt. Met het uitbreiden van het assortiment worden er in de loop der jaren diverse nieuwe BV’s opgericht. In 1976 start Doove Ripple Services BV met de verkoop van de eerste generaEe an E-decubitus</p>
+                </div>
+            </div>
+            <div class="order-2 w-[2px] ml-[8px] lg:ml-[unset] h-full relative hidden md:block">
+                <div class="bg-[#ED701F] w-[2px] absolute top-[0%] bottom-[50%] left-0 right-0"></div>
+                <div class="absolute bg-[#ED701F] w-[9px] h-[9px] mx-auto rounded-full top-[calc(50%-4.5px)] left-[-3.5px]"></div>
+            </div>
+            <div class="w-full md:w-[146px] lg:w-[412px] xl:w-[490px] hidden md:block order-2 lg:order-1 text-left lg:text-right">
+                <h2 class="font-satoshi font-bold md:text-21 md:leading-28 lg:text-22 leading-31 xl:text-25 xl:leading-35 text-[#332E2A]">Doove Ripple Services BV</h2>
+            </div>
+        </div>
+        <div class="absolute top-0 left-0 right-0 bottom-0 z-[0]">
+            <img src="/wp-content/themes/doove-theme/img/local/test.png" alt="" class="min-h-full min-w-full object-cover object-center-center opacity-[0.2]">
+        </div>
+         <div class="absolute top-0 left-0 right-0 bottom-0 z-[1]">
+            <div class="h-full w-full bg-white opacity-[0.17]"></div>
+        </div>
+    </section>
+
+<?php endif; ?>
---
+++ page-templates/over-ons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template name: Contact
+ */
+?>
+
+<?php get_header(); ?>
+    <main class="mt-[82px] md:mt-[71px] lg:mt-[68px]">
+        <?php the_content(); ?>
+    </main>
+
+<?php get_footer(); ?>
