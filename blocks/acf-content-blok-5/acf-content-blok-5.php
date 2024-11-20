4f6862b41a54d746ec340cc9a31674952cfee890
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-11-20 08:49:07 +0100

    UPDATE THEMA

--- blocks/acf-content-blok-5/acf-content-blok-5.php
+++ blocks/acf-content-blok-5/acf-content-blok-5.php
@@ -4,7 +4,7 @@ if (isset($block['data']['preview_image_help'])): /* rendering in inserter previ
 else: ?>
     <!-- CONTENT BLOK 5 -->
     <section class="bg-<?php the_field('achtergrond_kleur');?> <?php the_field('padding_top');?> <?php the_field('padding_bottom');?>">
-        <div class="container min-h-[200px]">
+        <div class="container">
             <?php $allowed_blocks_text = ['acf/acf-element-wysiwyg-editor', 'acf/acf-element-videos', 'acf/acf-element-button' ]; ?> <InnerBlocks allowedBlocks="<?php echo esc_attr( wp_json_encode( $allowed_blocks_text ) ); ?>" />
         </div>
     </section>
--- functions.php
+++ functions.php
@@ -23,10 +23,11 @@
 |
 */
 function add_theme_scripts() {
+     $version = file_exists(get_template_directory() . '/version.txt') ? file_get_contents(get_template_directory() . '/version.txt') : '1.0';
     wp_enqueue_style( 'swiper',  'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), '1.3', 'all');
-    wp_enqueue_script( 'swiper', get_template_directory_uri() . '/script/swiper.js', array(), 1.3, true);
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), 1.3, true);
+    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+    wp_enqueue_script( 'swiper', get_template_directory_uri() . '/script/swiper.js', array(), $version, true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), $version, true);
   }
   add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 /*
@@ -40,11 +41,12 @@ function add_theme_scripts() {
 */
 
 function load_custom_wp_admin_style(){
-    wp_enqueue_style( 'gutenberg', get_template_directory_uri() . '/gutenberg.css', array(), '1.3', 'all');
+     $version = file_exists(get_template_directory() . '/version.txt') ? file_get_contents(get_template_directory() . '/version.txt') : '1.0';
+    wp_enqueue_style( 'gutenberg',  'https://hostdashboard.nl/devdocs/css/gutenberg.css');
     wp_enqueue_style( 'swiper',  'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), '1.3', 'all');
-    wp_enqueue_script( 'swiper', get_template_directory_uri() . '/script/swiper.js', array(), 1.3, true);
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), 1.3, true);
+    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+    wp_enqueue_script( 'swiper', get_template_directory_uri() . '/script/swiper.js', array(), $version, true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), $version, true);
  
 }
 add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
@@ -264,102 +266,102 @@ function acf_icon_url( $path_suffix ) {
 */
 
 
-/*
- * Function for post duplication. Dups appear as drafts. User is redirected to the edit screen
- */
-function rd_duplicate_post_as_draft(){
-  global $wpdb;
-  if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'rd_duplicate_post_as_draft' == $_REQUEST['action'] ) ) ) {
-    wp_die('No post to duplicate has been supplied!');
-  }
-  /*
-   * Nonce verification
-   */
-  if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
-    return;
-  /*
-   * get the original post id
-   */
-  $post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
-  /*
-   * and all the original post data then
-   */
-  $post = get_post( $post_id );
-  /*
-   * if you don't want current user to be the new post author,
-   * then change next couple of lines to this: $new_post_author = $post->post_author;
-   */
-  $current_user = wp_get_current_user();
-  $new_post_author = $current_user->ID;
-  /*
-   * if post data exists, create the post duplicate
-   */
-  if (isset( $post ) && $post != null) {
-    /*
-     * new post data array
-     */
-    $args = array(
-      'comment_status' => $post->comment_status,
-      'ping_status'    => $post->ping_status,
-      'post_author'    => $new_post_author,
-      'post_content'   => $post->post_content,
-      'post_excerpt'   => $post->post_excerpt,
-      'post_name'      => $post->post_name,
-      'post_parent'    => $post->post_parent,
-      'post_password'  => $post->post_password,
-      'post_status'    => 'draft',
-      'post_title'     => $post->post_title,
-      'post_type'      => $post->post_type,
-      'to_ping'        => $post->to_ping,
-      'menu_order'     => $post->menu_order
-    );
-    /*
-     * insert the post by wp_insert_post() function
-     */
-    $new_post_id = wp_insert_post( $args );
-    /*
-     * get all current post terms ad set them to the new post draft
-     */
-    $taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
-    foreach ($taxonomies as $taxonomy) {
-      $post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
-      wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
-    }
-    /*
-     * duplicate all post meta just in two SQL queries
-     */
-    $post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
-    if (count($post_meta_infos)!=0) {
-      $sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
-      foreach ($post_meta_infos as $meta_info) {
-        $meta_key = $meta_info->meta_key;
-        if( $meta_key == '_wp_old_slug' ) continue;
-        $meta_value = addslashes($meta_info->meta_value);
-        $sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
-      }
-      $sql_query.= implode(" UNION ALL ", $sql_query_sel);
-      $wpdb->query($sql_query);
-    }
-    /*
-     * finally, redirect to the edit post screen for the new draft
-     */
-    wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
-    exit;
-  } else {
-    wp_die('Post creation failed, could not find original post: ' . $post_id);
-  }
-}
-add_action( 'admin_action_rd_duplicate_post_as_draft', 'rd_duplicate_post_as_draft' );
-/*
- * Add the duplicate link to action list for post_row_actions
- */
-function rd_duplicate_post_link( $actions, $post ) {
-  if (current_user_can('edit_posts')) {
-    $actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
-  }
-  return $actions;
-}
-add_filter( 'post_row_actions', 'rd_duplicate_post_link', 10, 2 );
+// /*
+//  * Function for post duplication. Dups appear as drafts. User is redirected to the edit screen
+//  */
+// function rd_duplicate_post_as_draft(){
+//   global $wpdb;
+//   if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'rd_duplicate_post_as_draft' == $_REQUEST['action'] ) ) ) {
+//     wp_die('No post to duplicate has been supplied!');
+//   }
+//   /*
+//    * Nonce verification
+//    */
+//   if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
+//     return;
+//   /*
+//    * get the original post id
+//    */
+//   $post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
+//   /*
+//    * and all the original post data then
+//    */
+//   $post = get_post( $post_id );
+//   /*
+//    * if you don't want current user to be the new post author,
+//    * then change next couple of lines to this: $new_post_author = $post->post_author;
+//    */
+//   $current_user = wp_get_current_user();
+//   $new_post_author = $current_user->ID;
+//   /*
+//    * if post data exists, create the post duplicate
+//    */
+//   if (isset( $post ) && $post != null) {
+//     /*
+//      * new post data array
+//      */
+//     $args = array(
+//       'comment_status' => $post->comment_status,
+//       'ping_status'    => $post->ping_status,
+//       'post_author'    => $new_post_author,
+//       'post_content'   => $post->post_content,
+//       'post_excerpt'   => $post->post_excerpt,
+//       'post_name'      => $post->post_name,
+//       'post_parent'    => $post->post_parent,
+//       'post_password'  => $post->post_password,
+//       'post_status'    => 'draft',
+//       'post_title'     => $post->post_title,
+//       'post_type'      => $post->post_type,
+//       'to_ping'        => $post->to_ping,
+//       'menu_order'     => $post->menu_order
+//     );
+//     /*
+//      * insert the post by wp_insert_post() function
+//      */
+//     $new_post_id = wp_insert_post( $args );
+//     /*
+//      * get all current post terms ad set them to the new post draft
+//      */
+//     $taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
+//     foreach ($taxonomies as $taxonomy) {
+//       $post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
+//       wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
+//     }
+//     /*
+//      * duplicate all post meta just in two SQL queries
+//      */
+//     $post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
+//     if (count($post_meta_infos)!=0) {
+//       $sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
+//       foreach ($post_meta_infos as $meta_info) {
+//         $meta_key = $meta_info->meta_key;
+//         if( $meta_key == '_wp_old_slug' ) continue;
+//         $meta_value = addslashes($meta_info->meta_value);
+//         $sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
+//       }
+//       $sql_query.= implode(" UNION ALL ", $sql_query_sel);
+//       $wpdb->query($sql_query);
+//     }
+//     /*
+//      * finally, redirect to the edit post screen for the new draft
+//      */
+//     wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
+//     exit;
+//   } else {
+//     wp_die('Post creation failed, could not find original post: ' . $post_id);
+//   }
+// }
+// add_action( 'admin_action_rd_duplicate_post_as_draft', 'rd_duplicate_post_as_draft' );
+// /*
+//  * Add the duplicate link to action list for post_row_actions
+//  */
+// function rd_duplicate_post_link( $actions, $post ) {
+//   if (current_user_can('edit_posts')) {
+//     $actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=rd_duplicate_post_as_draft&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this item" rel="permalink">Duplicate</a>';
+//   }
+//   return $actions;
+// }
+// add_filter( 'post_row_actions', 'rd_duplicate_post_link', 10, 2 );
 
 
 
@@ -863,259 +865,6 @@ function custom_navbar_categories() {
 
 
 
-  
-/*
-|--------------------------------------------------------------------------
-| Wordpress menu
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-
-function customize_dashboard_menu() {
-    global $menu;
-
-    // Vervang "super admin" door de gebruikersnaam die je wilt tonen.
-    $allowed_user = 'kevin';
-
-    // Haal de huidige ingelogde gebruiker op.
-    $current_user = wp_get_current_user();
-    $current_user_login = $current_user->user_login;
-
-    // Verberg specifieke menu-onderdelen voor alle gebruikers behalve "kevin".
-    if ($current_user_login !== $allowed_user) {
-        // Hier kun je de volledige URL/querystrings vinden van de menu-onderdelen die je wilt verbergen:
-        $hidden_menu_items_by_url = array(
-            'edit.php',
-            'edit.php?post_type=acf-field-group',
-            'edit-comments.php',
-            'themes.php',
-            'plugins.php',
-            // 'users.php',
-            'options-general.php',
-            'tools.php',
-            'admin.php?page=ai1wm_export'
-            // Voeg hier andere URL's/querystrings toe van de items die je wilt verbergen op basis van de URL.
-        );
-
-        // Hier kun je de classes vinden van de menu-onderdelen die je wilt verbergen:
-        $hidden_menu_items_by_class = array(
-            'toplevel_page_wppusher', 
-            'toplevel_page_ai1wm_export',
-            'menu-top toplevel_page_mlang',
-            'toplevel_page_rank-math',
-            'toplevel_page_zci_settings',
-            'menu-top toplevel_page_edit?post_type=filter-set',
-		'toplevel_page_wp-mail-smtp',
-            // Voeg hier andere classes toe van de items die je wilt verbergen op basis van de class.
-        );
-
-        foreach ($menu as $key => $item) {
-            // Verberg op basis van URL/querystring.
-            if (in_array($item[2], $hidden_menu_items_by_url)) {
-                unset($menu[$key]);
-            }
-
-            // Verberg op basis van class.
-            foreach ($hidden_menu_items_by_class as $class) {
-                if (strpos($item[4], $class) !== false) {
-                    unset($menu[$key]);
-                    break;
-                }
-            }
-        }
-    }
-}
-
-add_action('admin_menu', 'customize_dashboard_menu');
-
-
-/*
-|--------------------------------------------------------------------------
-| Wordpress topbar
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-function add_custom_admin_bar_styles() {
-    // Controleren of de gebruiker is ingelogd
-    if (is_user_logged_in()) {
-        // Gebruiker met de gebruikersnaam "xxx" uitsluiten
-        $user = wp_get_current_user();
-        if ($user->user_login === 'xxx') {
-            return;
-        }
-
-        // Voeg hier de CSS-styling toe voor de menu-items die je wilt aanpassen
-        $custom_styles = "
-            #wp-admin-bar-comments { display: none !important; }
-            #wp-admin-bar-customize { display: none !important; }
-            #wp-admin-bar-new-content { display: none !important; }
-            #wp-admin-bar-rank-math { display: none !important; }
-            #dashboard_primary { display: none !important; }
-            #dashboard_quick_press { display: none !important; }
-            #dashboard_activity  { display: none !important; }
-            #welcome-panel { display: none !important; }
-            #dashboard_site_health { display: none !important; }
-            #rg_forms_dashboard { display: none !important; }
-            #menu-posts { display: none !important; }
-	    #rank_math_dashboard_widget { display: none !important; }
-     		#wp_mail_smtp_reports_widget_lite { display: none !important; }
-	    #toplevel_page_litespeed { display: none !important; }
-     	#toplevel_page_wp-mail-smtp { display: none !important; }
-            /* Voeg hier meer CSS-styling toe indien nodig */
-        ";
-
-        // Voeg de CSS-styling toe aan zowel de front-end als het WordPress-dashboard
-        echo '<style type="text/css">' . $custom_styles . '</style>';
-        echo '<style type="text/css" id="custom-admin-bar-styles">' . $custom_styles . '</style>';
-    }
-}
-add_action('wp_head', 'add_custom_admin_bar_styles');
-add_action('admin_head', 'add_custom_admin_bar_styles');
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Wordpress footer
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-
-function vervang_dashboard_footer_tekst() {
-    echo 'Attention Seekers';
-}
-
-add_filter('admin_footer_text', 'vervang_dashboard_footer_tekst');
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Wordpress admin URL
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-// Functie voor het doorverwijzen van "/backend" naar "/wp-login.php"
-function redirect_backend_to_wp_login() {
-    if ($_SERVER['REQUEST_URI'] == '/backend') {
-        wp_redirect(wp_login_url());
-        exit;
-    }
-}
-add_action('init', 'redirect_backend_to_wp_login');
-
-
-
-/*
-|--------------------------------------------------------------------------
-| E-mailadres verzenden van mails
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-// function custom_wp_mail_from($original_email_address) {
-//     // Vervang 'jouw@emailadres.com' door het gewenste specifieke e-mailadres
-//     return 'noreply@doove.nl';
-// }
-
-// function custom_wp_mail_from_name($original_email_from) {
-//     // Vervang 'Jouw Naam' door de gewenste afzender naam
-//     return 'Doove Care Groep';
-// }
-
-// add_filter('wp_mail_from', 'custom_wp_mail_from');
-// add_filter('wp_mail_from_name', 'custom_wp_mail_from_name');
-
-
-/*
-|--------------------------------------------------------------------------
-| Hide Super Admin
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-function exclude_user_kevin_from_users_list($query) {
-    if (!is_admin()) {
-        return; // We voeren deze actie alleen uit in de backend
-    }
-
-    $current_user = wp_get_current_user();
-
-    // Controleer of de huidige gebruiker "super admin" is
-    if ($current_user->user_login === 'kevin') {
-        return; // "super admin" kan zijn eigen gebruikersgegevens zien
-    }
-
-    // Haal de huidige gebruiker op
-    $current_user_id = $current_user->ID;
-
-    // Haal de gebruiker "super admin" op
-    $kevin_user = get_user_by('login', 'kevin');
-
-    // Controleer of "super admin" bestaat en niet dezelfde is als de huidige gebruiker
-    if ($kevin_user && $current_user_id !== $kevin_user->ID) {
-        // Voeg een voorwaarde toe aan de gebruikersquery om "super admin" te verbergen voor andere gebruikers
-        $query->query_vars['exclude'] = array($kevin_user->ID);
-    }
-}
-add_action('pre_get_users', 'exclude_user_kevin_from_users_list');
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Hide auteur
-|--------------------------------------------------------------------------
-|
-| 
-| 
-|
-*/
-
-
-function verwijder_auteur_en_reacties_kolommen($columns) {
-    // Controleer of de 'auteur' kolom aanwezig is in de array van kolommen
-    if (isset($columns['author'])) {
-        // Verwijder de 'auteur' kolom uit de array
-        unset($columns['author']);
-    }
-
-    // Controleer of de 'reacties' kolom aanwezig is in de array van kolommen
-    if (isset($columns['comments'])) {
-        // Verwijder de 'reacties' kolom uit de array
-        unset($columns['comments']);
-    }
-
-    return $columns;
-}
-add_filter('manage_posts_columns', 'verwijder_auteur_en_reacties_kolommen');
-add_filter('manage_pages_columns', 'verwijder_auteur_en_reacties_kolommen');
-
-
-
-
 
 
 function rewrite_image_urls_js() {
@@ -1144,3 +893,12 @@ add_filter('xmlrpc_enabled', '__return_false');
 
 
 add_action('wp_head', 'rewrite_image_urls_js');
+
+// Disable pingbacks
+add_filter('xmlrpc_methods', 'disable_pingback');
+add_filter('pre_option_default_ping_status', '__return_false');
+
+function disable_pingback($methods) {
+    unset($methods['pingback.ping']);
+    return $methods;
+}
